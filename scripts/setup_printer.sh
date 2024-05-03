6b7313db2af7fc7d996704ca36ff58a8c653ca03
Ryu Siwoo <thezoot3@gmail.com>
2024-05-03 10:03:28 +0900

    bash script

--- scripts/setup_printer.sh
+++ scripts/setup_printer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in ./printers/*; do
+find ./printers -type f -exec {} \; | while read -r file; do
   echo "Starting Klipper and Moonraker for $file"
   docker-compose -f $file/docker-compose.yml up -d
 done
