c17f14b47f4df9b0810db8e43ea2166a86f4ee55
Ryu Siwoo <thezoot3@gmail.com>
2024-05-04 00:10:21 +0900

    bash

--- scripts/setup_printer.sh
+++ scripts/setup_printer.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+source ./scripts/parse_yaml.sh
+
+eval $(parse_yaml ../config.yml "config_")
+
+docker network create -d macvlan \
+  --subnet=$config_outerNetwork_subnet \
+  --ip-range=$config_outerNetwork_ipRange \
+  --gateway=$config_outerNetwork_gateway \
+  -o parent=$config_outerNetwork_parent \
+  3dp_outerNetwork
+
+for file in ../printers/*; do
+  echo "Stopping Loaded Klipper and Moonraker for $file"
+  docker-compose -f $file/docker-compose.yml down -d
+done
+
 for file in ../printers/*; do
   echo "Starting Klipper and Moonraker for $file"
   docker-compose -f $file/docker-compose.yml up -d
