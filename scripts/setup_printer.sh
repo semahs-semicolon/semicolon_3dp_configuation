9bcd697838f889b4ddea4cb93375c1585262375f
Ryu Siwoo <thezoot3@gmail.com>
2024-05-08 07:59:07 +0900

    one ip

--- scripts/setup_printer.sh
+++ scripts/setup_printer.sh
@@ -4,13 +4,6 @@ source ./parse_yaml.sh
 
 eval $(parse_yaml ../config.yaml "config_")
 
-docker network create -d macvlan \
-  --subnet=$config_outerNetwork_subnet \
-  --ip-range=$config_outerNetwork_ipRange \
-  --gateway=$config_outerNetwork_gateway \
-  -o parent=$config_outerNetwork_parent \
-  3dp_outerNetwork
-
 for dir in ../printers/*; do
   echo "Stopping Klipper and Moonraker for $dir"
   docker-compose -f $dir/docker-compose.yml down 
