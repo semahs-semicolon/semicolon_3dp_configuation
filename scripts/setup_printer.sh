65648a0644e613df3b70f941586bf4014b2ec200
Ryu Siwoo <thezoot3@gmail.com>
2024-05-13 23:44:27 +0900

    add stop service func

--- scripts/setup_printer.sh
+++ scripts/setup_printer.sh
@@ -3,6 +3,11 @@
 source ./parse_yaml.sh
 
 eval $(parse_yaml ../config.yaml "config_")
+for dir in ../printers/*; do
+  echo "Stopping Klipper and Moonraker Service for $dir"
+  systemctl stop klipper-$dir.service
+  systemctl stop moonraker-$dir.service
+done
 
 for dir in ../printers/*; do
   echo "Stopping Klipper and Moonraker for $dir"
